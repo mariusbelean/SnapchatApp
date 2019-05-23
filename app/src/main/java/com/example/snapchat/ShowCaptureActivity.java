dbcc35ee48de4e2d2a02743d1f9fbee09bc8b218
Marius <mariusibelean@gmail.com>
2019-05-23 14:52:27 +0300

    new-updates Added SaveCaptureToDb

--- app/src/main/java/com/example/snapchat/ShowCaptureActivity.java
+++ app/src/main/java/com/example/snapchat/ShowCaptureActivity.java
@@ -3,13 +3,33 @@ package com.example.snapchat;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.snapchat.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShowCaptureActivity extends AppCompatActivity {
+
+    String Uid;
+    Bitmap rotateBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +45,62 @@ public class ShowCaptureActivity extends AppCompatActivity {
 
             Bitmap decodeBitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
 
-            Bitmap rotateBitmap =  rotate (decodeBitmap);
+            rotateBitmap =  rotate (decodeBitmap);
 
             image.setImageBitmap(rotateBitmap);
         }
 
+        Uid = FirebaseAuth.getInstance().getUid();
+
+        Button mStory = findViewById(R.id.story);
+        mStory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveToStories();
+            }
+        });
+
+    }
+
+    private void saveToStories() {
+        final DatabaseReference userStoryDb = FirebaseDatabase.getInstance().getReference().child("users").child(Uid).child("story");
+        final String key = userStoryDb.push().getKey();
+
+        StorageReference filePath = FirebaseStorage.getInstance().getReference().child("captures").child(key);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        rotateBitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+        byte[] dataToUpload = baos.toByteArray();
+        UploadTask uploadTask = filePath.putBytes(dataToUpload);
+
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Task<Uri> imageUrl = taskSnapshot.getStorage().getDownloadUrl(); //need to check if this works!!!!!!!!!
+
+                Long currentTimeStamp = System.currentTimeMillis();
+                Long endTimeStamp = currentTimeStamp + (24*60*60*1000);
+
+                Map<String, Object> mapToUpload = new HashMap<>();
+                mapToUpload.put("imageUrl", imageUrl.toString());
+                mapToUpload.put("timestampBeg", currentTimeStamp);
+                mapToUpload.put("timestampEnd", endTimeStamp);
+
+                userStoryDb.child(key).setValue(mapToUpload);
+
+                finish();
+                return;
+
+            }
+        });
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                finish();
+                return;
+            }
+        });
     }
 
     private Bitmap rotate(Bitmap decodeBitmap) {
